efa59009de2e571aa13ec2debc5942818abce9a1
amreo <amreo@live.it>
2017-12-03 18:25:56 +0100

    Fixed bug array2string conversion

--- application/views/orientapolis.php
+++ application/views/orientapolis.php
@@ -9,12 +9,12 @@
       su "calcola!".
     </p>
     <form method="post">
-      <?php foreach ($domande as $domanda): ?>
+      <?php foreach ($domande as $domandaID => $domanda): ?>
         <?php echo $domanda["testo"] ?><br />
-        <input type="radio" name="<?php echo $domanda ?>" value="r1"><?php echo $domanda["r1"] ?></input><br />
-        <input type="radio" name="<?php echo $domanda ?>" value="r2"><?php echo $domanda["r2"] ?></input><br />
-        <input type="radio" name="<?php echo $domanda ?>" value="r3"><?php echo $domanda["r3"] ?></input><br />
-        <input type="radio" name="<?php echo $domanda ?>" value="r4"><?php echo $domanda["r4"] ?></input><br />
+        <input type="radio" name="<?php echo $domandaID ?>" value="r1"><?php echo $domanda["r1"] ?></input><br />
+        <input type="radio" name="<?php echo $domandaID ?>" value="r2"><?php echo $domanda["r2"] ?></input><br />
+        <input type="radio" name="<?php echo $domandaID ?>" value="r3"><?php echo $domanda["r3"] ?></input><br />
+        <input type="radio" name="<?php echo $domandaID ?>" value="r4"><?php echo $domanda["r4"] ?></input><br />
       <?php endforeach; ?>
     </form>
   </body>
