bf3ed2cdb291d19ef6b9c056d0d0d3c4032124a2
amreo <amreo@live.it>
2017-12-03 17:56:10 +0100

    Added basic homepage and controller

---
+++ application/views/orientapolis.php
@@ -0,0 +1,12 @@
+<DOCTYPE html>
+<html>
+  <head>
+      <title>Orientapolis</title>
+  </head>
+  <body>
+    <p>
+      Per calcolare la tua posizione politica, rispondere le domande e cliccare
+      su "calcola!".
+    </p>
+  </body>
+</html>
