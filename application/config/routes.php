e9560a5b117ba83f969f50516144678651e63ae1
amreo <amreo@live.it>
2017-12-03 17:59:15 +0100

    Fixed default controller route

--- application/config/routes.php
+++ application/config/routes.php
@@ -10,7 +10,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Typically there is a one-to-one relationship between a URL string
 | and its corresponding controller class/method. The segments in a
 | URL normally follow this pattern:
-|
+|orientapolis
 |	example.com/class/method/id/
 |
 | In some instances, however, you may want to remap this relationship
@@ -49,6 +49,6 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | Examples:	my-controller/index	-> my_controller/index
 |		my-controller/my-method	-> my_controller/my_method
 */
-$route['default_controller'] = 'welcome';
+$route['default_controller'] = 'orientapolis';
 $route['404_override'] = '';
 $route['translate_uri_dashes'] = FALSE;
