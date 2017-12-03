1db8361ee9119e54c4e7a8a4acc492c14bc77435
amreo <amreo@live.it>
2017-12-03 18:23:14 +0100

    Modified the view

--- application/controllers/Orientapolis.php
+++ application/controllers/Orientapolis.php
@@ -10,7 +10,19 @@ class Orientapolis extends CI_Controller{
 
   function index()
   {
-    $this->load->view("orientapolis");
+    $data["domande"]["d1"]["testo"] = "La libertà è una necessità";
+    $data["domande"]["d1"]["r1"] = "Fortemente d'accordo";
+    $data["domande"]["d1"]["r2"] = "D'accordo";
+    $data["domande"]["d1"]["r3"] = "Disaccordo";
+    $data["domande"]["d1"]["r4"] = "Fortemente disaccordo";
+
+    $data["domande"]["d2"]["testo"] = "È necessario censurare le notizie scomode al governo";
+    $data["domande"]["d2"]["r1"] = "Fortemente d'accordo";
+    $data["domande"]["d2"]["r2"] = "D'accordo";
+    $data["domande"]["d2"]["r3"] = "Disaccordo";
+    $data["domande"]["d2"]["r4"] = "Fortemente disaccordo";
+
+    $this->load->view("orientapolis", $data);
   }
 
 }
--- application/views/orientapolis.php
+++ application/views/orientapolis.php
@@ -5,8 +5,17 @@
   </head>
   <body>
     <p>
-      Per calcolare la tua posizione politica, rispondere le domande e cliccare
+      Per calcolare la tua posizione politica, rispondere alle domande e cliccare
       su "calcola!".
     </p>
+    <form method="post">
+      <?php foreach ($domande as $domanda): ?>
+        <?php echo $domanda["testo"] ?><br />
+        <input type="radio" name="<?php echo $domanda ?>" value="r1"><?php echo $domanda["r1"] ?></input><br />
+        <input type="radio" name="<?php echo $domanda ?>" value="r2"><?php echo $domanda["r2"] ?></input><br />
+        <input type="radio" name="<?php echo $domanda ?>" value="r3"><?php echo $domanda["r3"] ?></input><br />
+        <input type="radio" name="<?php echo $domanda ?>" value="r4"><?php echo $domanda["r4"] ?></input><br />
+      <?php endforeach; ?>
+    </form>
   </body>
 </html>
