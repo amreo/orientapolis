8abb92a0ec4066e4534df8d3869e1ff2bf3a829e
amreo <amreo@live.it>
2017-12-20 10:51:20 +0100

    Moved data from model to data/questions.json

--- application/models/QuestionData.php
+++ application/models/QuestionData.php
@@ -12,99 +12,10 @@ class QuestionData extends CI_Model{
   public function getQuestions()
   {
     //test data
-    $questions = array(
-      "data" => array (
-        "x" => array (
-          "offset" => 0,
-          "multiplier" => 1,
-        ),
-        "y" => array (
-          "offset" => 0,
-          "multiplier" => 1,
-        ),
-      ),
-      "questions" => array (
-        "domande_generali" => array (
-          "text" => "Cultura generale",
-          "questions" => array (
-            "d1" => array (
-              "text" => "È necessario essere liberi",
-              "answers" => array (
-                "r1" => array (
-                  "text" => "fortemente d'accordo",
-                  "y" => 0.8 ),
-                "r2" => array (
-                  "text" => "d'accordo",
-                  "y" => 0.5 ),
-                "r3" => array (
-                  "text" => "disaccordo",
-                  "y" => -0.5 ),
-                "r4" => array (
-                  "text" => "fortemenente disaccordo",
-                  "y" => -1 )
-                )
-            ),
-            "d2" => array (
-              "text" => "Non dobbiamo accogliere i migranti",
-              "answers" => array (
-                "r1" => array (
-                  "text" => "fortemente d'accordo",
-                  "x" => 0.7 ),
-                "r2" => array (
-                  "text" => "d'accordo",
-                  "x" => 0.3 ),
-                "r3" => array (
-                  "text" => "disaccordo",
-                  "x" => -0.2 ),
-                "r4" => array (
-                  "text" => "fortemenente disaccordo",
-                  "x" => -0.5 )
-                )
-              ),
-            )
-          ),
-          "domande_sociali" => array (
-            "text" => "Temi sociali",
-            "questions" => array (
-              "d1" => array (
-                "text" => "In uno stato civile l’aborto dovrebbe essere sempre legale",
-                "answers" => array (
-                  "r1" => array (
-                    "text" => "fortemente d'accordo",
-                    "y" => 0.5 ),
-                  "r2" => array (
-                    "text" => "d'accordo",
-                    "y" => 0.3 ),
-                  "r3" => array (
-                    "text" => "disaccordo",
-                    "y" => 0 ),
-                  "r4" => array (
-                    "text" => "fortemenente disaccordo",
-                    "y" => 0.7 )
-                  )
-              ),
-              "d2" => array (
-                "text" => "In Italia la situazione della donna è paritaria con quella degli uomini.",
-                "answers" => array (
-                  "r1" => array (
-                    "text" => "fortemente d'accordo",
-                    "x" => 0.8 ),
-                  "r2" => array (
-                    "text" => "d'accordo",
-                    "x" => 0.3 ),
-                  "r3" => array (
-                    "text" => "disaccordo",
-                    "x" => -0.2 ),
-                  "r4" => array (
-                    "text" => "fortemenente disaccordo",
-                    "x" => 0.8 )
-                )
-              )
-           )
-        )
-      )
-    );
-
+    $filename = "data/questions.json";
+    $handle = fopen($filename, "r");
+    $json = fread($handle, filesize($filename));
+    $questions = json_decode($json, true);
     return $questions;
   }
 }
